9801cc577aea6600526b2a44743f63d31022e07c
LEI-122712 <jpgao1@iscte-iul.pt>
2026-03-04 18:17:31 +0000

    logica compra bue fixe eu sou bue fixe por ter feito isto

--- Semana 1/script.js
+++ Semana 1/script.js
@@ -3,6 +3,9 @@ function increment(id){
     let qt=parseInt(elemento.innerText);
     elemento.innerText=qt+1;
     calculateTotal('qt1', 'qt2', 'qt3', 'total');
+    const sucessoCompra=document.getElementById("text1");
+    sucessoCompra.innerText ="";
+
 }
 
 function decrement(id){
@@ -11,6 +14,8 @@ function decrement(id){
     if(qt>0){
         elemento.innerText=qt-1;
         calculateTotal('qt1', 'qt2', 'qt3', 'total');
+        const sucessoCompra=document.getElementById("text1");
+        sucessoCompra.innerText ="";
     }
 }
 
@@ -31,8 +36,19 @@ function calculateTotal(id1, id2, id3, idTotal){
 }
 
 function sucessoCompra(){
-    document.getElementById("btn1").remove();
+
     const elemento=document.getElementById("text1");
-    elemento.innerText ="Compra realizada com sucesso!";
+    let elemento1=document.getElementById("qt1");
+    let elemento2=document.getElementById("qt2");
+    let elemento3=document.getElementById("qt3");
+    let qt1=parseInt(elemento1.innerText);
+    let qt2=parseInt(elemento2.innerText);
+    let qt3=parseInt(elemento3.innerText);
+    if(qt1>0 || qt2>0 || qt3>0){
+        elemento1.innerText=0;
+        elemento2.innerText=0;
+        elemento3.innerText=0;
+        elemento.innerText ="Compra realizada com sucesso!";
+    }
 
 }
