4cdb55142bd6bcffcba3439fd5456432174f20a7
José Maria Martins <jose.maria.martins@protonmail.com>
2026-03-12 15:06:00 +0000

    Header Done

--- semana4/src/Header.jsx
+++ semana4/src/Header.jsx
@@ -1,16 +1,23 @@
-import{ Container, Button} from'react-bootstrap'
+import { Container, Button } from 'react-bootstrap'
 
-const Header= () => {
-    return(
-          <header>
-            <h1>EKLEKTIKFEST LISBOA</h1>
-            <nav>
-                <a href="index.html"><img src="logo.png" alt="Logo"/></a>
-                <a class="navlink ativo" href="index.html">Página Inicial</a> |
-                <a class="navlink" href="candidatura.html">Candidatura</a> |
-                <a class="navlink" href="comprarBilhetes.html">Comprar Bilhetes</a>
-            </nav>
+const Header = () => {
+    return (
+        <header className="fixed-top" style={{ backgroundColor: "#fdfbd4"}}>
+            <Container className="text-center py-3">
+                <h1>EKLEKTIKFEST LISBOA</h1>
+                <nav className="d-flex justify-content-center align-items-stretch gap-3">
+                    <a href="/">
+                        <img src="logo.png" alt="Logo"/> 
+                    </a>
+                    <a href="/" className="btn h-100 py-3 btn-dark">Página Inicial</a>
+                    <span className="align-self-center">|</span>
+                    <a href="candidatura.html" className="btn h-100 py-3 btn-light">Candidatura</a>
+                    <span className="align-self-center">|</span>
+                    <a href="comprarBilhetes.html" className="btn h-100 py-3 btn-light">Comprar Bilhetes</a>
+                </nav>
+            </Container>
         </header>
     )
-    }
+}
+
 export default Header;
--- semana4/src/main.jsx
+++ semana4/src/main.jsx
@@ -4,7 +4,6 @@ import './index.css'
 import App from './App.jsx'
 import Header from './Header.jsx'
 import"bootstrap/dist/css/bootstrap.min.css";
-import{ Container, Button} from'react-bootstrap'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
