fb7486368af3d88916c56c2f965b6c2e94e1916e
LEI-122712 <jpgao1@iscte-iul.pt>
2026-03-12 14:27:35 +0000

    tirei um p estarnho

---
+++ semana4/src/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer= () => {
+    return (
+        <footer>
+            <h2>Contactos</h2>
+            Telefone: <a href="tel:+351219875784">219875784</a><br/>
+            Email: <a href="mailto:eklektikfest@ekfest.pt">eklektikfest@ekfest.pt</a>
+            <h2>Eventos relacionados</h2>
+            <a href="https://eklektikfestbarcelona.com">Eklektikfest Barcelona</a><br/>
+            <a href="https://eklektikfestrio.com">Eklektikfest Rio</a>
+        </footer>
+)
+}
+export default Footer;
--- semana4/src/main.jsx
+++ semana4/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Header from './Header.jsx'
+import Noticias from './Noticias.jsx'
 import"bootstrap/dist/css/bootstrap.min.css";
 import{ Container, Button} from'react-bootstrap'
 
@@ -10,5 +11,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
     <App />
+      <Noticias/>
   </StrictMode>,
 )
