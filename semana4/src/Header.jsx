70553dcbe46726f6e7b5b6e1976db51bdc6a68ca
José Maria Martins <jose.maria.martins@protonmail.com>
2026-03-12 13:39:40 +0000

    images

---
+++ semana4/src/Header.jsx
@@ -0,0 +1,14 @@
+constHeader= () => {
+    return(
+          <header>
+            <h1>EKLEKTIKFEST LISBOA</h1>
+            <nav>
+                <a href="index.html"><img src="dados/logo.png" alt="Logo"></a>
+                <a class="navlink ativo" href="index.html">Página Inicial</a> |
+                <a class="navlink" href="candidatura.html">Candidatura</a> |
+                <a class="navlink" href="comprarBilhetes.html">Comprar Bilhetes</a>
+            </nav>
+        </header>
+    )
+    }
+exportdefaultHeader;
