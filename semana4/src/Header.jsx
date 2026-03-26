9ed0206d72614cc11939ace34c89af054d28837a
Joana Pegado <joanapguerra2005@gmail.com>
2026-03-26 14:24:01 +0000

    pequena correcao escrita

--- semana4/src/Header.jsx
+++ semana4/src/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
             <div className="container">
                 <h1 className="display-5 mb-3">EKLEKTIKFEST LISBOA</h1>
                 <nav className="d-flex justify-content-center align-items-center gap-3">
-                    <img src="/logo.png" onClick={() => navigate("/")} alt="Logo" height="40" style={{ cursor: "pointer"} }/>
+                    <img src="/logo.png" onClick={() => navigate("/")} alt="Logo" style={{ cursor: "pointer", height: '40px'}}/>
                     <button onClick={() => navigate("/")} className="btn btn-outline-dark botao-texto-castanho">Página Inicial</button>
                     <button onClick={() => navigate("/galeria")} className="btn btn-outline-dark botao-texto-castanho">Galeria</button>
                     <button onClick={() => navigate("/inquerito")} className="btn btn-outline-dark botao-texto-castanho">Inquérito</button>
