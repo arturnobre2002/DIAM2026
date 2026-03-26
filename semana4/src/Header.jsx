6c3f29fb21fc6187d2c80cc9be4a949998921ecc
Joana Pegado <joanapguerra2005@gmail.com>
2026-03-26 14:20:18 +0000

    route feito

--- semana4/src/Header.jsx
+++ semana4/src/Header.jsx
@@ -8,9 +8,7 @@ const Header = () => {
             <div className="container">
                 <h1 className="display-5 mb-3">EKLEKTIKFEST LISBOA</h1>
                 <nav className="d-flex justify-content-center align-items-center gap-3">
-                    <button onClick={() => navigate("/")}>
-                        <img src="/logo.png" alt="Logo" height="40" />
-                    </button>
+                    <img src="/logo.png" onClick={() => navigate("/")} alt="Logo" height="40" style={{ cursor: "pointer"} }/>
                     <button onClick={() => navigate("/")} className="btn btn-outline-dark botao-texto-castanho">Página Inicial</button>
                     <button onClick={() => navigate("/galeria")} className="btn btn-outline-dark botao-texto-castanho">Galeria</button>
                     <button onClick={() => navigate("/inquerito")} className="btn btn-outline-dark botao-texto-castanho">Inquérito</button>
