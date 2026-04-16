033556212e778bbb13bd3b8bc50d5395c20f803d
Joana Pegado <joanapguerra2005@gmail.com>
2026-04-16 15:39:57 +0100

    semana 7 alinea a)

--- semana7/process_data.py
+++ semana7/process_data.py
@@ -1,5 +1,12 @@
-def create_question(question_text, options_list):
+from votacao.models import Questao, Opcao
+from django.utils import timezone
 
+options_list=[['Opcao A', 9], ['Opcao B', 0]]
 
-
+def create_question(question_text):
+    q = Questao.objects.create(questao_texto=question_text, pub_data=timezone.now())
+    q.save()
     for opcao in options_list:
+        q.opcao_set.create(opcao_texto=opcao[0], votos=opcao[1])
+
+create_question("Questao 1")
