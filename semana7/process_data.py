998253d4f57bdd850e285bcb0b364690d80a3da6
Joana Pegado <joanapguerra2005@gmail.com>
2026-04-16 15:54:49 +0100

    semana 7 alinea b) INACABADA

--- semana7/process_data.py
+++ semana7/process_data.py
@@ -1,7 +1,7 @@
 from votacao.models import Questao, Opcao
 from django.utils import timezone
 
-options_list=[['Opcao A', 9], ['Opcao B', 0]]
+#options_list=[['Opcao A', 9], ['Opcao B', 0]]
 
 def create_question(question_text):
     q = Questao.objects.create(questao_texto=question_text, pub_data=timezone.now())
@@ -9,4 +9,10 @@ def create_question(question_text):
     for opcao in options_list:
         q.opcao_set.create(opcao_texto=opcao[0], votos=opcao[1])
 
-create_question("Questao 1")
+#create_question("Questao 1")
+
+options_list=[['Rock', 0],['Metal', 0],['Pop',0],['Classica',0],['Rap',0],['Eletronica',0]]
+create_question("Qual o teu genero de musica favorito?")
+
+options_list=[['Alentejano' ,0], ['Acoriano',0],['Portuense',0], ['Madeirense',0], ['Algarvio',0], ['Lisboeta',0]]
+create_question("Qual o melhor sotaque portugues?")
